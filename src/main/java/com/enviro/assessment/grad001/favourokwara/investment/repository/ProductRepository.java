91158d1b784ea223084384b2e0317319cd673294
Favour Okwara <faokwar022@student.wethinkcode.co.za>
2024-01-25 08:48:13 +0000

    test: :white_check_mark: Add tests for the Product Repository

--- src/main/java/com/enviro/assessment/grad001/favourokwara/investment/repository/ProductRepository.java
+++ src/main/java/com/enviro/assessment/grad001/favourokwara/investment/repository/ProductRepository.java
@@ -1,9 +1,32 @@
 package com.enviro.assessment.grad001.favourokwara.investment.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.enviro.assessment.grad001.favourokwara.investment.model.Product;
 
-public interface ProductRepository extends CrudRepository<Product, Long> {
+/*
+ * ProductRepository interface responsible for managing Product entities.
+ * Extends JpaRepository to provide CRUD operation on our entities.
+ */
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    /**
+     * Retrieve list of all products associated with the specified investor.
+     * @param investorId The ID of the investor
+     * @return A list of products associated with the specified investor.
+     */
+    List<Product> findAllByInvestorId(Long investorId);
+
+    /**
+     * Find products associated with a specified investor by its ID. 
+     * @param productId  The ID of the product.
+     * @param investorId The ID of the investor.
+     * @return An optional with the found products.
+     */
+    Optional<Product> findByIdAndInvestorId(Long productId, Long investorId);
 }
---
+++ src/test/java/com/enviro/assessment/grad001/favourokwara/investment/repository/ProductRepositoryTest.java
@@ -0,0 +1,78 @@
+package com.enviro.assessment.grad001.favourokwara.investment.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+import com.enviro.assessment.grad001.favourokwara.investment.model.Investor;
+import com.enviro.assessment.grad001.favourokwara.investment.model.Product;
+import com.enviro.assessment.grad001.favourokwara.investment.model.ProductType;
+
+
+@DataJpaTest
+
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class ProductRepositoryTest {
+
+    @Autowired
+    InvestorRepository investorRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    public void ProductRepositoryTest_FindAllByInvestorId_ReturnsProducts() {
+        Long productId = 1L;
+
+        Investor investor = investorRepository.save(new Investor("James", "Bond", "jamesbnd007@email.com", "+12-34-567-8901", LocalDate.of(1960, 10, 17)));
+        
+        Product product = new Product("Savings", ProductType.SAVINGS, 50000.0);
+        product.setInvestor(investor);
+
+        productRepository.save(product);
+
+        Optional<Product> result = productRepository.findByIdAndInvestorId(product.getId(), investor.getId());
+
+        assertTrue(result.isPresent());
+        assertEquals(product.getId(), result.get().getId());
+    }
+
+    @Test
+    public void ProductRepositoryTest_FindByIdAndInvestorId_ReturnsProducts() {
+        Long productId = 1L;
+    
+        Investor investor = investorRepository.save(new Investor("James", "Bond", "jamesbnd007@email.com", "+12-34-567-8901", LocalDate.of(1960, 10, 17)));
+        
+        Product product = new Product("Savings", ProductType.SAVINGS, 50000.0);
+        product.setInvestor(investor);
+    
+        Product savedProduct = productRepository.save(product);
+    
+        Optional<Product> result = productRepository.findByIdAndInvestorId(product.getId(), investor.getId());
+    
+        assertTrue(result.isPresent());
+        assertEquals(savedProduct.getId(), result.get().getId());
+        assertEquals(investor.getId(), result.get().getInvestor().getId());
+        assertEquals(savedProduct.getName(), result.get().getName());
+    }
+
+    @Test
+    public void ProductRepositoryTest_FindByIdAndInvestorId_ReturnsEmptyOptionalWhenNotFound() {
+        Long investorId = 1L;
+        Long productId = 1L;
+
+        Optional<Product> result = productRepository.findByIdAndInvestorId(productId, investorId);
+
+        assertTrue(result.isEmpty());
+    }
+}
